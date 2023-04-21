dd52996c8c0f9bac06b01056c201e39f0fd393d4
Manas Baroi <manasbaroi6699@gmail.com>
2023-04-21 16:59:14 +0530

    backend + members section

--- Frontend/src/componants/Home/Home.jsx
+++ Frontend/src/componants/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, Component } from "react";
 import "../Home/Home.css";
-import c1 from "../../assets/c-1.jpg";
-import c2 from "../../assets/c-2.jpg";
-import c3 from "../../assets/c-3.jpg";
+import c1 from "../../assets/c-4.jpg";
+import c2 from "../../assets/c-5.jpg";
+import c3 from "../../assets/c-6.jpg";
 import Members from "../Members.jsx";
 import Notice from "../Notice";
 import Tpo from "../Tpo";
@@ -14,89 +14,28 @@ const Home = () => {
     <>  
       <div className="bg">
         
-        <div className="container my-4">
-          <div
-            id="carouselExampleDark"
-            className="carousel carousel-dark slide"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-indicators">
-              <button
-                type="button"
-                data-bs-target="#carouselExampleDark"
-                data-bs-slide-to="0"
-                className="active"
-                aria-current="true"
-                aria-label="Slide 1"
-              ></button>
-              <button
-                type="button"
-                data-bs-target="#carouselExampleDark"
-                data-bs-slide-to="1"
-                aria-label="Slide 2"
-              ></button>
-              <button
-                type="button"
-                data-bs-target="#carouselExampleDark"
-                data-bs-slide-to="2"
-                aria-label="Slide 3"
-              ></button>
-            </div>
-            <div className="carousel-inner">
-              <div className="carousel-item active" data-bs-interval="10000">
-                <img src={c1} className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                  <h2>First slide label</h2>
-                  <p>
-                    Some representative placeholder content for the first slide.
-                  </p>
-                </div>
-              </div>
-              <div className="carousel-item" data-bs-interval="2000">
-                <img src={c2} className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                  <h2>Second slide label</h2>
-                  <p>
-                    Some representative placeholder content for the second
-                    slide.
-                  </p>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <img src={c3} className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                  <h2>Third slide label</h2>
-                  <p>
-                    Some representative placeholder content for the third slide.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleDark"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleDark"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
+        <div className="container-sm my-4">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={c1} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={c2} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={c3} class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
         </div>
       </div>
 
--- Frontend/src/componants/Members.jsx
+++ Frontend/src/componants/Members.jsx
@@ -1,93 +1,84 @@
 import React from "react";
-import "./Home/Home.css";
+import styles from "./members.module.css";
 import semo from "../assets/semo2.png";
-import "./Home/Home.css";
+
 const Members = () => {
   return (
     <>
       <div className="bg">
-        <div class="container">
-          <div className="my-5">
-            <h1 className="header">Members</h1>
-          </div>
-          <div class="row">
-            <div class="col">
-              <div class=" p-3 mb-5 ">
-                <div class="rounded">
-                  <div class="card-body">
-                    <img class="middle" src={semo} alt="" />
-                    <p class="card-text align-middle">
-                      With supporting text below
-                    </p>
-                    <a href="#" class="btn btn-warning">
-                      View Details
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class=" p-3 mb-5 ">
-                <div class="rounded">
-                  <div class="card-body">
-                    <img class="middle" src={semo} alt="" />
-                    <p class="card-text align-middle">
-                      With supporting text below
-                    </p>
-                    <a href="#" class="btn btn-warning">
-                      View Details
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class=" p-3 mb-5 ">
-                <div class="rounded">
-                  <div class="card-body">
-                    <img class="middle" src={semo} alt="" />
-                    <p class="card-text align-middle">
-                      With supporting text below
-                    </p>
-                    <a href="#" class="btn btn-warning">
-                      View Details
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class=" p-3 mb-5 ">
-                <div class="rounded">
-                  <div class="card-body">
-                    <img class="middle" src={semo} alt="" />
-                    <p class="card-text align-middle">
-                      With supporting text below
-                    </p>
-                    <a href="#" class="btn btn-warning">
-                      View Details
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <h1 className="header">Members</h1>
       </div>
+
       <div className="bg">
-        <div className="container">
-          <p>
-          GCETT- Berhampore carries a legacy of 90 years as one of the premier institutes of India. The technology is continuously changing the world & it becomes a challenging task to cater the needs of the industry, but we are proud to say that GCETT-B has faced the challenge successfully. This is possible due to outstanding efforts of our faculty and of the bright students who are selected after a stringent process. Our alumni have achieved tremendous success in all spheres and this bears an eloquent testimony to our efforts.
+        <div className="container my-4">
+          <div class="row centered">
+            <div class="col">
+              <div className={styles.card}>
+              <div className="my-2">
+              <img src={semo} alt="" />
+              </div>
+              <p className="my-2">Manas Baroi</p>
+              <p className="my-2">manasbaroi6699@gmail.com</p>
+              <p className="my-2">📞01234567890</p>
+              </div>
+            </div>
+            <div class="col order-5">
+            <div className={styles.card}>
+            <div className="my-2">
+              <img src={semo} alt="" />
+              </div>
+              <p className="my-2">Manas Baroi</p>
+              <p className="my-2">manasbaroi6699@gmail.com</p>
+              <p className="my-2">📞01234567890</p>
+              </div>
+            </div>
+            <div class="col order-1">
+            <div className={styles.card}>
+            <div className="my-2">
+              <img src={semo} alt="" />
+              </div>
+              <p className="my-2">Manas Baroi</p>
+              <p className="my-2">manasbaroi6699@gmail.com</p>
+              <p className="my-2">📞01234567890</p>
+              </div>
+            </div>
+          </div>
 
-We take pride in cordially inviting you to participate in our endeavor and look forward to welcoming you to the recruitment process for our be loving students.
-
-Training and Placement Section (TPO), GCETT-Berhampore is responsible for conducting all training & placement related activities of the institute. The integral part of the TPO is a team of student coordinators from all courses of the college who work tirelessly to ensure that top notch opportunities are brought to the students & manages all interactions between the visiting companies and the colleges. It works round the clock to improve the placements and internships scenario of the college. TPO section try to give the best facilities and support possible to the recruiting companies. The details on facilities can be viewed here. For more queries contact us.
-
-TPO section is jointly working with other West Bengal govt colleges by conducting on-campus drive and pooled campus drive. Out TPO Students team will always help to the TPO section for arranging the hospitalities , travelling and other facilities provided to the visiting companies. A group of faculty members from various departments are working together for this TPO section. These tremendous effort by our students and faculty members, our students are achieving their goal.
-
-Non Placed students are continuously getting opportunities for the placement after completion of their course. TPO cell sent continuously mail to those students for any kind of placement notification and finally near about 100% students going to be placed..
-
-Placed Students are notified through mail for experience candidate recruitment of IT/Software , Core & Other companies.
+          <p className="my-5">
+            GCETT- Berhampore carries a legacy of 90 years as one of the premier
+            institutes of India. The technology is continuously changing the
+            world & it becomes a challenging task to cater the needs of the
+            industry, but we are proud to say that GCETT-B has faced the
+            challenge successfully. This is possible due to outstanding efforts
+            of our faculty and of the bright students who are selected after a
+            stringent process. Our alumni have achieved tremendous success in
+            all spheres and this bears an eloquent testimony to our efforts. We
+            take pride in cordially inviting you to participate in our endeavor
+            and look forward to welcoming you to the recruitment process for our
+            be loving students. Training and Placement Section (TPO),
+            GCETT-Berhampore is responsible for conducting all training &
+            placement related activities of the institute. The integral part of
+            the TPO is a team of student coordinators from all courses of the
+            college who work tirelessly to ensure that top notch opportunities
+            are brought to the students & manages all interactions between the
+            visiting companies and the colleges. It works round the clock to
+            improve the placements and internships scenario of the college. TPO
+            section try to give the best facilities and support possible to the
+            recruiting companies. The details on facilities can be viewed here.
+            For more queries contact us. TPO section is jointly working with
+            other West Bengal govt colleges by conducting on-campus drive and
+            pooled campus drive. Out TPO Students team will always help to the
+            TPO section for arranging the hospitalities , travelling and other
+            facilities provided to the visiting companies. A group of faculty
+            members from various departments are working together for this TPO
+            section. These tremendous effort by our students and faculty
+            members, our students are achieving their goal. Non Placed students
+            are continuously getting opportunities for the placement after
+            completion of their course. TPO cell sent continuously mail to those
+            students for any kind of placement notification and finally near
+            about 100% students going to be placed.. Placed Students are
+            notified through mail for experience candidate recruitment of
+            IT/Software , Core & Other companies.
           </p>
         </div>
       </div>
